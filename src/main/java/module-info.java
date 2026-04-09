d9eed127395ed63c2a8bf843764a1ea0539fcb61
Tulio <tquintanaa@miumg.edu.gt>
2026-04-09 14:41:24 -0600

    mejora

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module org.example.projectgestaltt {
     requires org.antlr.antlr4.runtime;
 
     opens org.example.projectgestaltt to javafx.fxml;
+    opens org.example.projectgestaltt.model to javafx.base;  // ← AGREGAR ESTA
     exports org.example.projectgestaltt;
     exports org.example.projectgestaltt.compiler;
     exports org.example.projectgestaltt.model;
